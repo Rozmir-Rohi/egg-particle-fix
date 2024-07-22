a894e912ec838877988878eaafcea193af9bb182
Rozmir <120696188+Rozmir-Rohi@users.noreply.github.com>
2024-07-22 16:40:52 +1000

    Added everything

---
+++ src/main/java/rozmir/EntityEggExtension.java
@@ -0,0 +1,59 @@
+package rozmir;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.projectile.EntityEgg;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+public class EntityEggExtension extends EntityEgg
+{
+    private int attackTimer;
+    
+    public EntityEggExtension(final World world) {
+        super(world);
+    }
+    
+    public EntityEggExtension(World world, EntityLivingBase thrower)
+    {
+        super(world, thrower);
+    }
+    
+    @Override
+    protected void onImpact(MovingObjectPosition p_70184_1_)
+    {
+        if (p_70184_1_.entityHit != null)
+        {
+            p_70184_1_.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 0.0F);
+        }
+
+        if (!this.worldObj.isRemote && this.rand.nextInt(8) == 0)
+        {
+            byte b0 = 1;
+
+            if (this.rand.nextInt(32) == 0)
+            {
+                b0 = 4;
+            }
+
+            for (int i = 0; i < b0; ++i)
+            {
+                EntityChicken entitychicken = new EntityChicken(this.worldObj);
+                entitychicken.setGrowingAge(-24000);
+                entitychicken.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
+                this.worldObj.spawnEntityInWorld(entitychicken);
+            }
+        }
+
+        for (int j = 0; j < 8; ++j)
+        {
+            this.worldObj.spawnParticle("iconcrack_344", this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+        }
+
+        if (!this.worldObj.isRemote)
+        {
+            this.setDead();
+        }
+    }	
+}
